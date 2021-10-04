1e64ace94b0c7dae69a679c0ed19443532d58d27
marcinstenka <mastenka@wp.pl>
2021-10-04 23:14:03 +0200

    1.1.1 - voucher added

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
     }
   }, [isMobile]);
   return (
-    <BrowserRouter basename="hotelnadmorskileba">
+    <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Hotel} />
         <Route exact path="/pokoje" component={Rooms} />
